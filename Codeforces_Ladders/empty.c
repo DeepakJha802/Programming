49920b5725811c5aa3bb51a022a30002418e1f13
DeepakJha802 <dkjha7646038831@gmail.com>
2022-04-07 14:22:19 +0530

    Update

--- Codeforces_Ladders/empty.c
+++ Codeforces_Ladders/empty.c
@@ -64,22 +64,77 @@
 //     }
 // }
 
+// #include <stdio.h>
+// int main(void)
+// {
+//     int t;
+//     scanf("%d", &t);
+//     int n;
+//     for (int i = 0; i < t; i++)
+//     {
+//         scanf("%d", &n);
+//         int fact = 1;
+//         for (int j = 1; j <= n; j++)
+//         {
+//             fact = fact * j;
+//         }
+//         printf("%d\n",fact);
+//     }
+// }
+
+// #include <stdio.h>
+// int main(void)
+// {
+//     int t ;
+//     scanf("%d",&t);
+//     for (int i = 0; i < t; i++)
+//     {
+//         int r , s;
+//         scanf("%d %d",&r,&s);
+//         if (r%2==0 && s%2==0)
+//         printf("%d\n",(s/2)*(r/2));
+//         else if (r%2!=0 && s%2!=0)
+//         printf("%d\n",(((s-1)/2)+1)*(((r-1)/2)+1));
+//         else if (r%2==0 && s%2!=0)
+//         printf("%d\n",((((s-1)/2)+1)*(r/2)));
+//         else if (r%2!=0 && s%2==0)
+//         printf("%d\n",(s/2)*((((r-1)/2)+1)));
+
+//     }
+// }
+
+// #include<stdio.h>
+// int main(void)
+// {
+//     int t ;
+//     scanf("%d",&t);
+//     for (int i = 0; i < t; i++)
+//     {
+//         int a , b , c , d , e;
+//         scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+//         if (a+b<=d && c<=e || b+c<=d && a<=e || a+c<=d && b<=e)
+//         printf("YES\n");
+//         else
+//         printf("NO\n");
+
+//     }
+// }
+
+// C Program to demonstrate that we can access a global
+// variable if we have a local variable with same name
 #include <stdio.h>
-int main(void)
+
+// Global variable x
+
+int main()
+int x = 50;
 {
-    int t;
-    scanf("%d", &t);
-    int n;
-    for (int i = 0; i < t; i++)
-    {
-        scanf("%d", &n);
-        int fact = 1;
-        for (int j = 1; j <= n; j++)
-        {
-            fact = fact * j;
-        }
-        printf("%d\n",fact);
-    }
+	// Local variable x
+	int x = 10;
+	{
+		extern int x;
+		printf("Value of global x is %d\n", x);
+	}
+	printf("Value of local x is %d\n", x);
+	return 0;
 }
-
-
