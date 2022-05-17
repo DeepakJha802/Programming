260fadb5462cb2617893b83089e07e39d794e7a2
DeepakJha802 <dkjha7646038831@gmail.com>
2022-05-18 04:59:43 +0530

    Update

--- Codeforces_Ladders/630A.c
+++ Codeforces_Ladders/630A.c
@@ -1,33 +1,34 @@
-#include <stdio.h>
-#include <math.h>
+// #include <stdio.h>
+// #include <math.h>
 
-int main(void)
-{
-    int n, m;
-    scanf("%d %d", &n, &m);
-    int a[n], b[m];
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &a[i]);
-    }
-    for (int i = 0; i < m; i++)
-    {
-        scanf("%d", &b[i]);
-    }
-    int result = 0;
-    for (int i = 0; i < n; i++)
-    {
-        int count = 0;
-        for (int j = 0; j < m; j++)
-        {
+// int main(void)
+// {
+//     int n, m;
+//     scanf("%d %d", &n, &m);
+//     int a[n], b[m];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &a[i]);
+//     }
+//     for (int i = 0; i < m; i++)
+//     {
+//         scanf("%d", &b[i]);
+//     }
+//     int result = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         int count = 0;
+//         for (int j = 0; j < m; j++)
+//         {
 
-            if (b[j] % a[i] == 0)
-                count++;
-        }
-        if (count == m)
-        {
-            result++;
-        }
-    }
-    printf("%d\n",result);
-}
+//             if (b[j] % a[i] == 0)
+//                 count++;
+//         }
+//         if (count == m)
+//         {
+//             result++;
+//         }
+//     }
+//     printf("%d\n",result);
+// }
+
