bcaa0627619bd5c00c15f30b0b2f4cdf74b624f1
DeepakJha802 <dkjha7646038831@gmail.com>
2022-05-25 20:35:35 +0530

    Update

--- Codeforces_Ladders/630A.c
+++ Codeforces_Ladders/630A.c
@@ -32,3 +32,22 @@
 //     printf("%d\n",result);
 // }
 
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d",&arr[i]);
+        }
+        
+    }
+}
