d1146675f74d5f3d8f9505cc52c85640e7ce6ff9
DeepakJha802 <dkjha7646038831@gmail.com>
2022-06-15 21:54:18 +0530

    Update

--- assignment/fractional_knapsack.c
+++ assignment/fractional_knapsack.c
@@ -1,46 +1,50 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    int c, n, c_w, item;
-    int used[10];
-    float t_p;
-    int i;
+    int a[499999];
 
-    printf("Enter the capacity of knapsack:\n");
-    scanf("%d", &c);
-
-    printf("Enter the number of items:\n");
+    int n;
     scanf("%d", &n);
-    int w[n], v[n];
-    printf("Enter the weight and value item:\n");
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d %d", &w[i], &v[i]);
-    }
-    for (i = 0; i < n; ++i)
-        used[i] = 0;
-    c_w = c;
-    while (c_w > 0)
-    {
-        item = -1;
-        for (i = 0; i < n; ++i)
-            if ((used[i] == 0) &&
-                ((item == -1) || ((float)v[i] / w[i] > (float)v[item] / w[item])))
-                item = i;
 
-        used[item] = 1;
-        c_w -= w[item];
-        t_p += v[item];
-        if (c_w >= 0)
-            printf("Added %d (%d Rs., %dKg) completely in knapsack. Space left: %d.\n", item + 1, v[item], w[item], c_w);
-        else
+    for (int i = (n - 1); i < ((2 * n) - 1); i++)
+        scanf("%d", &(a[i]));
+
+    memmove(a, (a + n), (n - 1) * sizeof (int));
+
+    unsigned long long ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int j = i;
+
+        while (j < (i + n - 1))
         {
-            int i_p = (int)((1 + (float)c_w / w[item]) * 100);
-            printf("Added %d%% (%d Rs., %dKg) of %d in knapsack.\n", i_p, v[item], w[item], item + 1);
-            t_p -= v[item];
-            t_p += (1 + (float)c_w / w[item]) * v[item];
+            int k = (i + n - 1);
+
+            while (1)
+            {
+                if (a[k] >= (k - j))
+                    break;
+
+                k--;
+            }
+
+            ans += 1;
+            j = k;
         }
     }
-    printf("Maximum value = %.2f\n", t_p);
+
+    printf("%llu\n", ans);
+
+    return 0;
 }
+
+// 0 - 2
+// 1 - 3
+// 2 - 4
+// 3 - 1
+// 4 - 2
+// 5 - 3
+// 6 - 4
