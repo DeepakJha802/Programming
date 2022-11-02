5496b770497415a04ff85ec368cd2caa02557053
DeepakJha802 <dkjha7646038831@gmail.com>
2022-11-02 11:21:47 +0530

    Update

--- Codeforces_Ladders/112A.c
+++ Codeforces_Ladders/112A.c
@@ -1,23 +1,41 @@
-#include<stdio.h>
+// #include<stdio.h>
+// int main(void)
+// {
+//     int t;
+//     scanf("%d",&t);
+//     while(t--)
+//     {
+//         int n;
+//         scanf("%d",&n);
+//         if (n % 7 == 0)
+//         {
+//             printf("%d\n",n);
+//         }
+//         else
+//         {
+//             int s = n / 7;
+//             if (((s + 1) * 7)% 10 == 0)
+//             {
+//                 printf("%d\n",(s + 1) * 7);
+//             }
+//         }
+//     }
+// }
+
+#include <stdio.h>
+
 int main(void)
 {
-    int t;
-    scanf("%d",&t);
-    while(t--)
-    {
-        int n;
-        scanf("%d",&n);
-        if (n % 7 == 0)
-        {
-            printf("%d\n",n);
-        }
-        else
-        {
-            int s = n / 7;
-            if (((s + 1) * 7)% 10 == 0)
-            {
-                printf("%d\n",(s + 1) * 7);
-            }
-        }
-    }
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    if (n <= 0)
+        goto loc_1;
+
+    loc_0: printf("Hello World\n"); --n;
+    if (n > 0)
+        goto loc_0;
+
+    loc_1: return 0;
 }
