7630651d89e4fa5b30d13ed64f43bcb56d5060d2
DeepakJha802 <dkjha7646038831@gmail.com>
2022-11-18 20:05:13 +0530

    Update

--- Codeforces_Ladders/469A.c
+++ Codeforces_Ladders/469A.c
@@ -4,27 +4,33 @@
 
 int main(void)
 {
-    int n;
-    scanf("%d", &n);
-    char a[n], b[n-1];
-    bool present = false;
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d %d",&a[i],&b[i]);
-    }
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] == n || b[i] == n)
-        {
-            present = true ;
-        }
-        else
-        present =  false ;
-    }
-    if (present)
-    printf("I become the guy.");
-    else
-    printf("Oh, my keyboard!");
+
+    int x = 3 , y = 5 , z = 10;
+    int p = ++z+y-y+z+x++;
+    printf("%d",p);
+
+
+    // int n;
+    // scanf("%d", &n);
+    // char a[n], b[n-1];
+    // bool present = false;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     scanf("%d %d",&a[i],&b[i]);
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (a[i] == n || b[i] == n)
+    //     {
+    //         present = true ;
+    //     }
+    //     else
+    //     present =  false ;
+    // }
+    // if (present)
+    // printf("I become the guy.");
+    // else
+    // printf("Oh, my keyboard!");
 }
 
 
